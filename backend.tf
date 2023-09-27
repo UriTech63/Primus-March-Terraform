c0d5ac8c5da68864bb9e2685c5016622e80be40c
UriTech63 <105933334+UriTech63@users.noreply.github.com>
2023-09-26 22:22:24 -0600

    Update backend.tf

---
+++ backend.tf
@@ -0,0 +1,21 @@
+resource "aws_s3_bucket" "primus-s3" {
+  bucket = "primus-s3"
+
+  tags = {
+    Name = "primus-s3"
+  }
+}
+
+
+
+terraform {
+  backend "s3" {
+    bucket = "primus-s3"
+    key    = "primus-tm-key/terraform-march-state"
+     region = "us-east-1"
+     # encrypt = "true"
+    dynamodb_table = "terraform-lock"
+  }
+}
+
+
